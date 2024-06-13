a5dd45bf002efff74e52bc0a42d1fd37f32ae309
bhargavimskr <bhargavimskr225@gmail.com>
2024-06-13 12:04:03 +0530

    Keep One Message

--- Study.CodingTracker1/Program.cs
+++ Study.CodingTracker1/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One More Modification");
-Console.WriteLine("Hello, C# Academy! One More Modification");Console.WriteLine("Hello,C#"); Console.WriteLine("Hello, C# Academy! One More Modification");
-Console.WriteLine("Hello, C# Academy! One More Modification");Console.WriteLine("Hello,C#"); Console.WriteLine("Hello, C# Academy! One More Modification");
-Console.WriteLine("Hello, C# Academy! One More Modification");Console.WriteLine("Hello,C#"); Console.WriteLine("Hello, C# Academy! One More Modification");
-Console.WriteLine("Hello, C# Academy! One More Modification");Console.WriteLine("Hello,C#"); Console.WriteLine("Hello, C# Academy! One More Modification");
-Console.WriteLine("Hello, C# Academy! One More Modification");Console.WriteLine("Hello,C#"); Console.WriteLine("Hello, C# Academy! One More Modification");
+Console.WriteLine("Hello, C# Academy! Fetching Details");
